3be1f3a22cc74dbfd5c548eea0eaf0b884d7687f
Aarif-khan-mohammad <142022266+Aarif-khan-mohammad@users.noreply.github.com>
2024-02-29 14:23:11 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+const CardItem = (props)=>{
+  const {cardInfo} = props;
+  const {job ,description, imgUrl} = cardInfo;
+
+  return(
+    <div className="Card-Item">
+      <div>
+        <h1 className="heading">{job}</h1>
+        <p className="paraa">{description}</p>
+      </div>
+      <img src={imgUrl} alt="img" className="img"/>
+    </div>
+  )
+}
+
+export default CardItem
